d8be32dfd858d08e8cdf6c59ef3ed53505c0d438
Eric Ruelas <eric.ruelas@karauctionservices.com>
2020-06-01 22:31:55 -0500

    finished tying order flow together

--- src/app/pages/home/OrderDetails.js
+++ src/app/pages/home/OrderDetails.js
@@ -1,22 +1,31 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import RunnerNavBar from "../../partials/content/RunnerNavBar";
 import { useLocation } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import { Card } from "react-bootstrap";
+import * as orders from "../../store/ducks/order.duck";
 
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
+
+
 function OrderDetails(props) {
   let query = useQuery();
   // eslint-disable-next-line eqeqeq
-  const thisOrder = props.orders.orders.filter(
-    (x) => x.orderNumber == query.get("orderId")
-  )[0];
-  console.log("Order Details props init", props);
-  console.log(thisOrder);
+  // const thisOrder = props.orders.orders.filter(
+  //   (x) => x.orderNumber == query.get("orderId")
+  // )[0];
+  console.log('props load')
+  console.log(props)
+  let data = Array.from(props.orders.orders);
+  console.log('list load')
+  console.log(data)
+  const thisOrder = data.filter(x => x.orderNumber === 1)[0]
+  console.log('order load')
+  console.log(thisOrder)
 
   const orderItems = thisOrder.items.map((item) => {
     return (
@@ -63,11 +72,52 @@ function OrderDetails(props) {
       </div>
 
       <div className={"p-4"}>
-        <Button type="submit">{thisOrder.orderStatus}</Button>
+        <Button onClick={handleStatusClick(thisOrder, 'Prepared', props)}>Prepared</Button>
       </div>
     </div>
   );
 }
+
+const handleStatusClick = (order, nav, props) => {
+  console.log('button click order')
+  console.log(order)
+
+  console.log('button click nav')
+  console.log(nav)
+
+  console.log('button click props')
+  console.log(props)
+
+  order.orderStatus = nav;
+  props.changeOrderStatus(order);
+}
+
+const UpdateOrderStatusButton = (thisOrder) => {
+  console.log('render button')
+  console.log(thisOrder)
+  //console.log(props)
+  switch (thisOrder.thisOrder.orderStatus) {
+    case 'Queue':
+      return <Button onClick={handleStatusClick(thisOrder, 'Prepared')}>Order Prepared</Button>;
+    case 'Prepared':
+    //  return <Button  onClick={handleStatusClick(props, thisOrder, 'Running')}>On My Way!</Button>;
+    case 'Running':
+    //   return (
+    //     <div>
+    //       <Button onClick={handleStatusClick(props, thisOrder, 'Completed')}>Delivered!</Button>;
+    //       <br/>
+    //       <br/>
+    // /     <Button onClick={handleStatusClick(props, thisOrder, 'Prepared')}>Can't Find Customer!</Button>;
+    //     </div>
+    //   )
+    case 'Completed':
+      return null;
+    default:
+      console.log(thisOrder.order)
+      return <p>failed to load button</p>
+  }
+}
+
 
 function mapStateToProps(state) {
   return {
@@ -75,4 +125,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(OrderDetails);
+export default connect(mapStateToProps, orders.actions)(OrderDetails);
